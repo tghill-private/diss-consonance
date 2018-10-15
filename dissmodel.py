632b5fdfe42903286521c5b5ae748ac9033e0e9a
tghill <timofhill@gmail.com>
2018-10-15 19:46:49 -0400

    First commit

---
+++ dissmodel.py
@@ -0,0 +1,88 @@
+"""
+    Implementation of the dissonance model from Sethares 1993
+
+    TODOi: figure out interface
+
+    Make a Timbre class to hold timbre information
+
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+## CONSTANTS
+
+a = 3.5
+b = 5.75
+
+
+def D(F, V):
+    """Compute Plomp-Levelt dissonance for given frequencies and amplitudes.
+
+    Compute d according to Sethares 1993.
+
+    Args:
+     * F: iterable of frequencies present
+     * V: iterable of relative amplitudes
+    """
+    D = 0
+    for i,fi in enumerate(F):
+        for j,fj in enumerate(F):
+            D += 0.5 * d(fi, fj, V[i], V[j])
+    return D
+
+def DF(F, V, alpha):
+    Da = 0
+    for i,fi in enumerate(F):
+        for j,fj in enumerate(F):
+            Da += d(fi, alpha*fj, V[i], V[j])
+
+    return D(F, V) + D(alpha*F, V) + Da 
+#    return Da
+
+def d(f1, f2, v1, v2):
+    dstar = 0.24
+    s1 = 0.0021
+    s2 = 19
+    s = dstar/(s1*f1 + s2)
+    exp1 = np.exp(- a * s * np.abs(f2-f1) )
+    exp2 = np.exp(- b * s * np.abs(f2-f1) )
+    return v1*v2*(exp1 - exp2)
+
+if __name__ == '__main__':
+    print(d(440, 440, 1, 1))
+    print(d(440, 442, 1, 1))
+    print(d(440, 440*3/2, 1, 1))
+
+    v1 = 1
+    v2 = 1
+    f1 = 440
+    F2 = np.linspace(f1, 2*f1, 1000)
+    diss = [d(f1, f2, v1, v2) for f2 in F2]
+
+    fig, ax = plt.subplots()
+    ax.plot(F2/f1, diss)
+    ax.set_xlabel('$\\alpha = \\frac{f_2}{f_1}$')
+    ax.set_ylabel('Perceived dissonance')
+    ax.set_title('Dissonance curve for $f_1 = 440$ Hz')
+    ax.grid()
+    fig.savefig('disscurve0.png', dpi = 400)
+
+    F = np.array([440*i for i in range(1, 6)])
+    V = [np.exp(np.log(0.88)*i) for i in range(5)]
+    print(F)
+    print(V)
+
+    fig, ax = plt.subplots()
+    alpha = np.linspace(1., 2.1, 1000)
+    disson = [DF(F, V, a) for a in alpha]
+
+    ax.plot(alpha, np.array(disson)/np.max(disson))
+    ax.set_xlabel('$\\Number of semitones$')
+    ax.set_ylabel('Perceived Dissonance')
+    ax.set_title('Dissonance curve for harmonic timbre')
+#    ax.set_xticks(np.linspace(1, 2, 13))
+    ax.set_xticks([2.0**(i/12.) for i in range(13)])
+    ax.set_xticklabels([str(i) for i in range(13)])
+    ax.grid()
+    fig.savefig('disscurve_harmonic.png', dpi = 400)
---
+++ timbre.py
@@ -0,0 +1,101 @@
+"""
+
+    timbre.py
+
+    Python module containing Instrument and Timbre classes.
+
+    An Instrument instance stores the frequency ratios the instrument always
+    produces; a Timbre instance stores the absolute frequencies present
+    when an Instrument is played at a given fundamental frequency.
+
+"""
+
+import numpy as np
+
+class Instrument:
+    """Instrument represents the partials of a simulated instrument.
+
+    Constructor Arguments:
+     * A: List of frequency ratios present. Float or iterable.
+     * V: Relative amplitudes of each frequency ratio. Float or iterable.
+
+    Optional Arguments:
+     * name: Name of the instrument for pretty printing
+
+    Methods:
+     * __call__: Allows calling instance as a function. Returns a Timbre
+                    instance corresponding to the instrument at the given
+                    fundamental frequency.
+    """
+    def __init__(self, A, V, name = ''):
+        """Instrument constructor.
+
+        A is (float or list of) frequency ratios present.
+        V is (float or list of) relative frequency amplitudes.
+
+        For example, if A = (1, 2, 3, 4) and V = (1, 0.5, 0.25, 0.125),
+        the instrument has the first 4 harmonics present at relative amplitudes
+        each 1/2 of the previous partial.
+        """
+        A = np.array(A)
+        V = np.array(V)
+
+        self.A = A
+        self.V = V
+        self.name = name if name else 'Instrument'
+
+    def __call__(self, f0):
+        """All calling of instrument instance as a function.
+
+        Returns a Timbre instance with fundamental frequency f0. Partials
+        are computed as f0 * alpha_i for each frequncy ratio alpha_i of the
+        instrument.
+        """
+        return Timbre(f0 * self.A, self.V, name = '%s (%s)' % (self.name, f0))
+
+    def __repr__(self):
+        Alist = ", ".join([str(a) for a in self.A])
+        Vlist = ", ".join([str(v) for v in self.V])
+        return "Timbre((%s), (%s), %s)" % (Alist, Vlist, self.name)
+
+    def __str__(self):
+        Alist = ", ".join([str(a) for a in self.A])
+        Vlist = ", ".join([str(v) for v in self.V])
+        return "%s: (%s), (%s)" % (self.name, Alist, Vlist)
+
+class Timbre:
+    """Timbre represents the frequency components present in a given note.
+    """
+    def __init__(self, F, V, name = ''):
+        F = np.array(F)
+        V = np.array(V)
+
+        self.F = F
+        self.V = V
+
+        self.name = name if name else 'Timbre'
+
+    def __iter__(self):
+        for (f, v) in zip (self.F, self.V):
+            return (f, v)
+
+    def __repr__(self):
+        Flist = ", ".join([str(f) for f in self.F])
+        Vlist = ", ".join([str(v) for v in self.V])
+        return "Timbre((%s), (%s), %s)" % (Flist, Vlist, name)
+
+    def __str__(self):
+        Flist = ", ".join([str(f) for f in self.F])
+        Vlist = ", ".join([str(v) for v in self.V])
+        return "%s: (%s), (%s)" % (self.name, Flist, Vlist)
+
+if __name__ == '__main__':
+    harmonic = Instrument((1, 2, 3, 4, 5), (1, 0.5, 0.25, 0.125, 0.125/2), 'Harmonix')
+
+    print(harmonic)
+
+    print(harmonic(440))
+
+    anharm = Instrument((1, np.sqrt(2), np.sqrt(5), np.sqrt(7)), (1, 0.8, 0.8**2, 0.8**3), 'Anharmonix')
+    print(anharm)
+    print(anharm(440))
