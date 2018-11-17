3e93ad10bbe3267dca8daba3c3fb80d89e05fe4a
tghill <timofhill@gmail.com>
2018-11-17 14:19:42 -0500

    Add transc partial stuff

--- code/curves.py
+++ code/curves.py
@@ -15,7 +15,7 @@ import numpy as np
 
 from model import model
 
-def dissonance_curve(T, figname, title = 'Dissonance curve'):
+def dissonance_curve(T, figname, title = 'Dissonance curve', xscale = 'semitone'):
     """Draw a dissonance curve.
 
     Labels the x-axis with semitones for 12 TET scale
@@ -26,15 +26,21 @@ def dissonance_curve(T, figname, title = 'Dissonance curve'):
 
     fig, ax = plt.subplots(figsize = (8, 4))
 
-    ax.set_title(title)
+    # ax.set_title(title)
 
-    ax.set_xlabel('Number of semitones')
     ax.set_ylabel('Perceived dissonance')
 
     ax.plot(alpha, dissonance)
 
-    ax.set_xticks([2**(i/12.) for i in range(13)])
-    ax.set_xticklabels([str(i) for i in range(13)])
+    if xscale == 'semitone':
+        ax.set_xticks([2**(i/12.) for i in range(13)])
+        ax.set_xticklabels([str(i) for i in range(13)])
+        ax.set_xlabel('Number of semitones')
+    else:
+        ax.set_xlabel('$\\alpha$')
+
     ax.grid()
 
     fig.savefig(figname, dpi = 600)
+
+    return (alpha, dissonance)
